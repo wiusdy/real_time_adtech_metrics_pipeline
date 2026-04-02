8f5b223191d66c6655f7c1bdab06d03018cf3bf1
William Sdayle Marins Silva <WS_MarinsSilva@freewheel.tv>
2026-04-02 12:53:03 -0300

    rollback in producer version

--- producer/producer.py
+++ producer/producer.py
@@ -4,15 +4,33 @@ import time
 
 from kafka import KafkaProducer
 
-producer = KafkaProducer(
-    bootstrap_servers="localhost:9092",
-    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
-)
 
-while True:
-    event = {"user_id": random.randint(1, 10), "value": random.random()}
+class Producer:
+    def __init__(self, bootstrap_servers="localhost:9092", topic="events"):
+        self.topic = topic
+        self.producer = KafkaProducer(
+            bootstrap_servers=bootstrap_servers,
+            value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+        )
 
-    producer.send("events", event)
-    print("Sent:", event)
+    def send(self, event: dict):
+        self.producer.send(self.topic, event)
+        print("Sent:", event)
 
-    time.sleep(1)
+    def generate_event(self):
+        return {
+            "user_id": random.randint(1, 10),
+            "value": random.random(),
+        }
+
+    def run(self, interval=1):
+        while True:
+            event = self.generate_event()
+            self.send(event)
+            time.sleep(interval)
+
+
+# 👇 Permite rodar como script
+if __name__ == "__main__":
+    producer = Producer()
+    producer.run()
